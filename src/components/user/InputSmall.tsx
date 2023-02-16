beee172712ea176bd55459a5459bddf4e23f3980
Rafael Pereira <rafaelsantospereira03@gmail.com>
2023-02-16 10:22:07 -0300

    feature: generate json application

--- src/components/user/InputSmall.tsx
+++ src/components/user/InputSmall.tsx
@@ -1,17 +1,98 @@
+import { useNode } from "@craftjs/core";
+import { useEffect, useState } from "react";
+
 interface InputSmall {
   label: string;
   placeholder: string;
+  fontSize?: number;
 }
 
-export function InputSmall({ label, placeholder }: InputSmall) {
+export function InputSmall({
+  label,
+  fontSize,
+  placeholder,
+  ...props
+}: InputSmall) {
+  const {
+    connectors: { connect, drag },
+    selected,
+    actions: { setProp },
+  }: any = useNode((state) => ({
+    selected: state.events.selected,
+    dragged: state.events.dragged,
+  }));
+
+  const [editable, setEditable] = useState(false);
+
+  useEffect(() => {
+    if (selected) {
+      return;
+    }
+
+    setEditable(false);
+  }, [selected]);
+
   return (
-    <label className="text-sm">
-      {label}
-      <input
-        className="w-full mt-1 h-12 px-4 rounded-lg border-2 border-gray-300 outline-none focus:ring-2 focus:ring-pink-500 transition-all placeholder:text-sm"
-        type="text"
-        placeholder={placeholder}
-      />
-    </label>
+    <div
+      {...props}
+      ref={(ref) => connect(drag(ref))}
+      onClick={() => selected && setEditable(true)}
+    >
+      <label className="text-sm">
+        {label}
+        <input
+          className="w-full mt-1 h-12 px-4 rounded-lg border-2 border-gray-300 outline-none focus:ring-2 focus:ring-pink-500 transition-all placeholder:text-sm"
+          type="text"
+          placeholder={placeholder}
+          onChange={(e) =>
+            setProp(
+              (props: any) =>
+                (props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, "")),
+              500
+            )
+          }
+          style={{ fontSize: `${fontSize}px` }}
+        />
+      </label>
+    </div>
   );
 }
+
+const InputMallSettings = () => {
+  const {
+    actions: { setProp },
+    fontSize,
+  }: any = useNode((node) => ({
+    fontSize: node.data.props.fontSize,
+  }));
+
+  return (
+    <div>
+      <label>
+        Tamanho da fonte
+        <input
+          type="range"
+          value={fontSize || 7}
+          step={7}
+          min={12}
+          max={30}
+          onChange={(e) =>
+            setProp((props: any) => (props.fontSize = e.target.value))
+          }
+          className="w-full mt-2"
+        />
+      </label>
+    </div>
+  );
+};
+
+export const InputMallDefaultProps = {
+  fontSize: 16,
+};
+
+InputSmall.craft = {
+  props: InputMallDefaultProps,
+  related: {
+    settings: InputMallSettings,
+  },
+};
--- src/contexts/JsonContext.tsx
+++ src/contexts/JsonContext.tsx
@@ -1,10 +1,15 @@
-import { createContext, Dispatch, ReactNode, useState } from "react";
+import { createContext, Dispatch, ReactNode, useEffect, useState } from "react";
+import lz from "lzutf8";
 
 interface JsonContextData {
   openJSONText: boolean;
   jsonText: string;
   setJsonText: Dispatch<string>;
   setOpenJSONText: Dispatch<boolean>;
+  setJsonLoad: Dispatch<string>;
+  jsonLoad: string;
+  setJsonTextBackup: Dispatch<string>;
+  jsonTextBackup: string;
 }
 
 interface JsonProviderProps {
@@ -15,7 +20,15 @@ export const JsonContext = createContext({} as JsonContextData);
 
 export function JsonProvider({ children }: JsonProviderProps) {
   const [openJSONText, setOpenJSONText] = useState(false);
+
   const [jsonText, setJsonText] = useState("");
+  const [jsonLoad, setJsonLoad] = useState("");
+  const [jsonTextBackup, setJsonTextBackup] = useState("");
+
+  useEffect(() => {
+    const json = lz.decompress(lz.decodeBase64(jsonText));
+    setJsonLoad(json);
+  }, [jsonText]);
 
   return (
     <JsonContext.Provider
@@ -24,6 +37,10 @@ export function JsonProvider({ children }: JsonProviderProps) {
         jsonText,
         setJsonText,
         setOpenJSONText,
+        setJsonLoad,
+        jsonLoad,
+        setJsonTextBackup,
+        jsonTextBackup,
       }}
     >
       {children}
